e16ea18ba8408a1c268f67b65b447e0fd39fa62d
Stephanie Chan <scychan@princeton.edu>
2015-10-16 16:12:32 -0400

    run_all_subjs.sh optionally accepts "models" input

--- trialbytrial/run_all_subjs.sh
+++ trialbytrial/run_all_subjs.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-models_short='Bayesian logBayesian additive mostP_voter'
+models=$1 # keep empty for all models
+
+models_short='Bayesian logBayesian additive mostP_voter mostleast2_voter most2_voter least2_voter'
 models_long='mostleast_voter feedbackRL logfeedbackRL'
+if [ -z $models ]; then
+    models="$models_short $models_long"
+fi
+
 subjnums='101 102 103 104 105 106 107 108 109 110 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 129 130 131 132 133 134'
 use_likelihood_estimates='0 1'
 ninits=10
 
-for subjnum in $subjnums; do
-    for ule in $use_likelihood_estimates; do
-	for model in $models_short; do
-	    submit_short run_model.m $model $subjnum $ule $ninits
-	done
-	for model in $models_long; do
-	    submit_long run_model.m $model $subjnum $ule $ninits
+for model in $models; do
+    for subjnum in $subjnums; do
+	for ule in $use_likelihood_estimates; do
+	    if [[ $models_short =~ $model ]]; then
+		submit_short run_model.m $model $subjnum $ule $ninits
+	    elif [[ $models_long =~ $model ]]; then
+		submit_long run_model.m $model $subjnum $ule $ninits
+	    else
+		echo "ERROR: $model is an invalid model"
+	    fi
 	done
     done
 done
