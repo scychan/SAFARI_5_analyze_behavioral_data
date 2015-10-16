4f4ea340b3868794ef9a4fd807e7044ba39626eb
Stephanie Chan <scychan@princeton.edu>
2015-10-16 17:27:12 -0400

    trialbytrial/compile_inits.m - not bothered by missing inits (issues a warning)

--- trialbytrial/compile_inits.m
+++ trialbytrial/compile_inits.m
@@ -9,15 +9,20 @@ allfits = struct;
 inits = nan(1,ninits);
 for i = 1:ninits
     % load iteration
-    temp = load(sprintf('%s/estliks%i_SFR%i_init%i',...
-        resultsdir,estliks,subjnum,i));
-    allfits(i).params = temp.allfits(i).params;
-    allfits(i).negloglik = temp.allfits(i).negloglik;
-    inits(i) = temp.inits(i);
-    
-    % update bestfit
-    if ~any(isnan(allfits(i).params)) && allfits(i).negloglik < bestfit.negloglik
-        bestfit = allfits(i);
+    resultfile = sprintf('%s/estliks%i_SFR%i_init%i.mat',...
+        resultsdir,estliks,subjnum,i);
+    if ~exist(resultfile,'file')
+        warning('%s does not exist',resultfile)
+    else
+        temp = load(resultfile);
+        allfits(i).params = temp.allfits(i).params;
+        allfits(i).negloglik = temp.allfits(i).negloglik;
+        inits(i) = temp.inits(i);
+        
+        % update bestfit
+        if ~any(isnan(allfits(i).params)) && allfits(i).negloglik < bestfit.negloglik
+            bestfit = allfits(i);
+        end
     end
 end
 
