3b4ca9fe86c05042d1826253599caaeb85ce241a
Stephanie Chan <scychan@princeton.edu>
2015-10-19 10:37:14 -0400

    TBT/compile_inits - recompute negloglik if params are imaginary/outside constraints

--- trialbytrial/compile_inits.m
+++ trialbytrial/compile_inits.m
@@ -20,8 +20,26 @@ for i = 1:ninits
         allfits(i).negloglik = temp.allfits(i).negloglik;
         inits(:,i) = temp.inits(:,i);
         
+        % re-compute the actual negloglik for the params if
+        % (a) negloglik is imaginary (b) params are outside constraints
+        [~,cons] = get_param_inits_cons(modelname,ninits);
+        outcons = cons.A * allfits(i).params > cons.B;
+        if ~isreal(allfits(i).negloglik) || any(outcons)
+            allfits(i).params = real(temp.allfits(i).params); % make params real
+            if any(outcons)
+                for icon = horz(find(outcons))
+                    conparam = cons.A(2,:) ~= 0;
+                    allfits(i).params(conparam) = cons.B(icon); % set param to closest constraint
+                end
+            end
+            data = get_data(modelname, subjnum, estliks);
+            pchoices_fun = get_pchoices_for_data(modelname,data);
+            allfits(i).negloglik = pchoices_fun(allfits(i).params);
+        end
+        
         % update bestfit
-        if ~any(isnan(allfits(i).params)) && allfits(i).negloglik < bestfit.negloglik
+        if ~any(isnan(allfits(i).params)) && isreal(allfits(i).negloglik) ...
+                && allfits(i).negloglik < bestfit.negloglik
             bestfit = allfits(i);
         end
     end
