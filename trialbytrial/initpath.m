3a5b46b2ee153a3987715f5a0b9c485cba8ff2cb
Stephanie Chan <scychan@princeton.edu>
2015-10-19 16:36:18 -0400

    new file:   TBT/initpath.m

---
+++ trialbytrial/initpath.m
@@ -0,0 +1,11 @@
+if ~exist('pchoices_Bayesian.m','file')
+    addpath('models');
+end
+
+if ~exist('get_subjnums.m','file')
+    addpath('../helpers')
+end
+
+if ~exist('get_ntrials.m','file')
+    addpath('helpers')
+end
