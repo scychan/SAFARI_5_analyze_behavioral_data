c35ffb52e7bc3b5cd7f1aa7e0c868f8edebc3c95
Stephanie Chan <scychan@princeton.edu>
2015-10-20 10:21:41 -0400

    TBT run_all_subjs.sh - allow subjnums and use_likelihood_estimates as inputs

--- trialbytrial/run_all_subjs.sh
+++ trialbytrial/run_all_subjs.sh
@@ -2,6 +2,8 @@
 
 models=$1 # keep empty to run all models
 ninits=$2
+subjnums=$3
+use_likelihood_estimates=$4
 
 models_short='zz Bayesian logBayesian additive Bayesian_recencyprimacy Bayesian_recencyprimacy_sameweight Bayesian_recency Bayesian_primacy mostP_voter mostleast2_voter most2_voter least2_voter feedbackRL_1alpha feedbackRL_correctalso_1alpha logfeedbackRL_1alpha zz'
 models_long='zz mostleast_voter feedbackRL feedbackRL_correctalso logfeedbackRL feedbackRL_recencyprimacy feedbackRL_recencyprimacy_sameweight feedbackRL_1alpha_recencyprimacy feedbackRL_1alpha_recencyprimacy_sameweight zz'
@@ -10,11 +12,18 @@ if [ -z "$models" ]; then
     models="$models_short $models_long $models_longlong"
 fi
 
-subjnums='101 102 103 104 105 106 107 108 109 110 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 129 130 131 132 133 134'
-use_likelihood_estimates='0 1'
 if [ -z "$ninits" ]; then
     ninits=10
 fi
+
+if [ -z "$subjnums" ]; then
+    subjnums='101 102 103 104 105 106 107 108 109 110 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 129 130 131 132 133 134'
+fi
+
+if [ -z "$use_likelihood_estimates" ]; then
+    use_likelihood_estimates='0 1'
+fi
+
 
 for model in $models; do
     for subjnum in $subjnums; do
