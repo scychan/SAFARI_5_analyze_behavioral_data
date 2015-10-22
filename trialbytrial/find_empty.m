992c99505c469d1d2e4809ea744fd4c7c85ec1ed
Stephanie Chan <scychan@princeton.edu>
2015-10-22 17:59:10 -0400

    TBT find_empty.m - now prints bash command for running missing subjects

--- trialbytrial/find_empty.m
+++ trialbytrial/find_empty.m
@@ -1,4 +1,4 @@
-function find_empty(modelname)
+function find_empty(modelname,ninit)
 
 % info
 initpath
@@ -6,16 +6,26 @@ subjnums = get_subjnums;
 nsubj = length(subjnums);
 resultsdir = sprintf('../../results/trialbytrial/fits_%s',modelname);
 
-% compile fits from all subjects
+% find the k+subj that are missing
 for k = 1:2
     use_likelihood_estimates = k-1;
+    
+    missing_subj = [];
     for isubj = 1:nsubj
         subjnum = subjnums(isubj);
         
         filename = sprintf('%s/estliks%i_SFR%i',resultsdir,use_likelihood_estimates,subjnum);
         temp = load(filename);
         if temp.bestfit.negloglik == Inf || ~isfield(temp.bestfit,'params')
-            fprintf('%s \n',filename)
+%             fprintf('%s \n',filename)
+            missing_subj = [missing_subj subjnum];
         end
+    end
+    
+    % print the required bash command
+    if ~isempty(missing_subj)
+        fprintf('bash run_all_subjs.sh %s %i ''',modelname,ninit)
+        fprintf('%i ',missing_subj)
+        fprintf(''' %i \n',use_likelihood_estimates)
     end
 end
